0e0fc3c54e445e4a34e7be0295d8da33099219d1
Andromeda <stroev.andrew@gmail.com>
2025-12-25 03:39:05 +0000

    Fix audio stacking and clean up CSS

--- src/components/RecordModule.svelte
+++ src/components/RecordModule.svelte
@@ -13,7 +13,7 @@
     let speed = $state(0);
     let searchSample = $state('');
     let searchPromise: Promise<Sample[]> | undefined = $state();
-    let sampleUrl: string | undefined = $state();
+    let sampleUrl = $state('');
 
     const controller = new PlaybackController();
 
@@ -63,46 +63,65 @@
 <div>
     <VinylRecord {controller} bind:speed img="https://picsum.photos/200" />
 
-    <input type="search" list="samples" bind:value={searchSample} />
-    <button
-        onclick={() => {
-            searchPromise = searchFreesound(searchSample);
-        }}>Search</button
-    >
-    <select bind:value={sampleUrl}>
-        {#if searchPromise}
-            {#await searchPromise}
-                <option>Searching...</option>
-            {:then samples}
-                {#each samples as sample}
-                    <option value={sample.url}>{sample.title}</option>
-                {/each}
-            {:catch}
-                <option>Search error</option>
-            {/await}
-        {:else}
-            <option>Select a sample...</option>
-        {/if}
-    </select>
+    <div class="my-3">
+        <div class="mb-2 flex gap-2">
+            <input
+                class="flex-1 rounded border-3 px-4 py-2"
+                type="search"
+                list="samples"
+                placeholder="Search for a sample..."
+                bind:value={searchSample}
+            />
+            <button
+                class="w-25 shrink-0 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+                onclick={() => {
+                    searchPromise = searchFreesound(searchSample);
+                }}>Search</button
+            >
+        </div>
+        <select
+            class="w-full rounded border-3 px-4 py-2"
+            bind:value={sampleUrl}
+        >
+            {#if searchPromise}
+                {#await searchPromise}
+                    <option value="" disabled selected>Searching...</option>
+                {:then samples}
+                    <option value="" disabled selected
+                        >{samples.length} samples found!</option
+                    >
+                    {#each samples as sample}
+                        <option value={sample.url}>{sample.title}</option>
+                    {/each}
+                {:catch}
+                    <option>Search error</option>
+                {/await}
+            {:else}
+                <option value="" disabled selected>Select a sample...</option>
+            {/if}
+        </select>
+    </div>
 
-    <br />
-
-    <input
-        type="range"
-        min="-5"
-        max="5"
-        step="0.00000000001"
-        defaultvalue="0"
-        bind:value={speed}
-        oninput={() => controller.resumePlayback()}
-    />
-    <input
-        type="number"
-        min="-5"
-        max="5"
-        step="0.01"
-        defaultvalue="0"
-        bind:value={speed}
-        oninput={() => controller.resumePlayback()}
-    />
+    <div class="flex gap-2">
+        <input
+            class="flex-1"
+            type="range"
+            min="-5"
+            max="5"
+            step="0.00000000001"
+            defaultvalue="0"
+            bind:value={speed}
+            oninput={() => controller.resumePlayback()}
+        />
+        <input
+            class="w-25 shrink-0 rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+            type="number"
+            min="-5"
+            max="5"
+            step="0.01"
+            defaultvalue="0"
+            bind:value={speed}
+            oninput={() => controller.resumePlayback()}
+        />
+    </div>
 </div>
