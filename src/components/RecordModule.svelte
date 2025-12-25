c2b0de36271f2bd748d8c07a68f8fce745d9fba0
Andromeda <stroev.andrew@gmail.com>
2025-12-25 01:51:16 +0000

    Add playable record

---
+++ src/components/RecordModule.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+    import { VinylRecord, PlaybackController } from 'virtual-vinyl';
+
+    let speed = $state(0);
+    const controller = new PlaybackController();
+
+    $effect(() => {
+        controller.loadFromUrl(
+            'https://cdn.freesound.org/previews/63/63054_216886-hq.mp3',
+            true,
+        );
+    });
+</script>
+
+<div class="bg-red-500">
+    <VinylRecord {controller} bind:speed img="https://picsum.photos/200" />
+    Speed: <input type="number" bind:value={speed} />
+</div>
